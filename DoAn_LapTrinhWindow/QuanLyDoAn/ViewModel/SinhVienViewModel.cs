8fa425775118a4498d173b1320f924268b8c43e1
tienduc18 <tienduc29072000@gmail.com>
2020-06-13 22:42:10 +0700

    Thay đổi: Bổ sung và thực thi hàm đổ dữ liệu và SinhVienViewModel
    Lý do: Chưa đổ được dữ liệu ngay sau khi cập nhật

---
+++ DoAn_LapTrinhWindow/QuanLyDoAn/ViewModel/SinhVienViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDoAn.ViewModel
+{
+    public class SinhVienViewModel
+    {
+        public string Mssv { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string QueQuan { get; set; }
+        public string GioiTinh { get; set; }
+        public string Khoa { get; set; }
+        public string ChuyenNganh { get; set; }
+        public string NamNhapHoc { get; set; }
+    }
+}
